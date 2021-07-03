b0122e230e3a4bde235e84633fc8704c45a70a3f
akmhmgc <akimuhamaguchi@icloud.com>
2021-07-03 18:03:56 +0900

    Add アーティストで検索

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+  require 'rspotify'
+  RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:secret_id])
+
   def home
+  end
+
+  def artists
+    if params[:search].present?
+    @searchartists = RSpotify::Artist.search(params[:search])
+    end
   end
 end
