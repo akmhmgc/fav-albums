225fc3efdb5b519110f07ef05fed8c0d76976f6d
akmhmgc <akimuhamaguchi@icloud.com>
2021-07-03 19:01:31 +0900

    Add #3 Configの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      './app/**/*.html.erb',
+      './app/**/*.js.erb',
+      './app/helpers/**/*.rb'
+    ],
+    options: {
+      safelist: ['a'],
+      keyframes: true,
+      fontFace: true,
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
