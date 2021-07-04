a63fbdb1af4b8fd8319925fca9f5e9c96c6d92bb
akmhmgc <akimuhamaguchi@icloud.com>
2021-07-04 11:28:54 +0900

    #5 Add ページネーション機能の追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
   def home; end
 
   def artists
-    @searchartists = RSpotify::Artist.search(params[:search]) if params[:search].present?
+    searchartists_array = RSpotify::Artist.search(params[:search])
+    @searchartists = Kaminari.paginate_array(searchartists_array).page(params[:page]).per(10) if params[:search].present?
   end
 end
