87e88c9617b8cdbfbade055659adb52cf6307dda
akmhmgc <akimuhamaguchi@icloud.com>
2021-07-04 17:38:56 +0900

    #2 Add リストにアーティストを追加する機能

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,7 @@
 class ArtistsController < ApplicationController
   require 'rspotify'
+  require 'active_support'
+  require 'active_support/core_ext'
   RSpotify.authenticate(Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:secret_id])
 
   def home; end
@@ -7,7 +9,19 @@ class ArtistsController < ApplicationController
   def index
     return if params[:search].blank?
 
-    searchartists_array = RSpotify::Artist.search(params[:search])
+    searchartists_array = RSpotify::Artist.search(params[:search]).map { |artist| spotify_to_hash(artist) }
     @searchartists = Kaminari.paginate_array(searchartists_array).page(params[:page]).per(10)
+  end
+
+  private
+
+  def spotify_to_hash(spotify_instance)
+    image_url = if spotify_instance.images.any?
+                  spotify_instance.images[1]["url"]
+                else
+                  "no_image.png"
+                end
+
+    { "id": spotify_instance.id, "name": spotify_instance.name, "image_url": image_url }.with_indifferent_access
   end
 end
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,14 +1,8 @@
 module ArtistsHelper
-  def artist_image_url(artist)
-    if artist.images.any?
-      artist.images[1]["url"]
-    else
-      "no_image.png"
-    end
+  def included_in_mylists?(artist)
+    return false unless session[:my_artists_list]
+
+    artist_ids_in_mylist = session[:my_artists_list].map { |artist_info| artist_info[:id] }
+    artist_ids_in_mylist.include?(artist["id"])
   end
-
-  #   アーティストがリストに追加されているかどうか
-#   def included_in_lists?(artist)
-
-#   end
 end
