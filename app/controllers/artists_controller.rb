1373c5b04ba4626f790f18bfdda65641b85988af
akmhmgc <akimuhamaguchi@icloud.com>
2021-07-11 18:46:19 +0900

    画面遷移方法の修正

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,9 +16,6 @@ class ArtistsController < ApplicationController
     @description = "#{@my_list.nickname}さんを構成するアーティストは#{artists_list.join('・')}です。"
     return unless flash[:notice]
 
-    @description_twitter = <<~MSG
-      #{@my_list.nickname}さんを構成するアーティストは%0a#{artists_list.join('%0a')}です。%0a
-      @Buffalo_G_7777 %20%23私を構成する5組のアーティスト%20 #{myFavArtistList_url(@my_list.to_param)}
-    MSG
+    @description_twitter = "#{@my_list.nickname}さんを構成するアーティストは%0a#{artists_list.join('%0a')}です。%0a@Buffalo_G_7777 %20%23私を構成する5組のアーティスト%20 #{myFavArtistList_url(@my_list.to_param)}"
   end
 end
