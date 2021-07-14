d4fd0a446961c62fbb617a8428894bc9b1bced91
akmhmgc <akimuhamaguchi@icloud.com>
2021-07-14 17:08:31 +0900

    MyArtistCookie attributionのテスト

--- spec/models/my_artist_cookie_spec.rb
+++ spec/models/my_artist_cookie_spec.rb
@@ -6,8 +6,21 @@ RSpec.describe MyArtistCookie, type: :model do
     expect(artist_cookie).to be_valid
   end
 
-  it "有効なモデルの作成" do
-    artist_cookie = FactoryBot.build(:my_artist_cookie)
-    expect(artist_cookie).to be_valid
+  it "idがない場合、無効である" do
+    artist_cookie = FactoryBot.build(:my_artist_cookie, id:"")
+    artist_cookie.valid?
+    expect(artist_cookie.errors.of_kind?(:id, :blank)).to be_truthy
+  end
+
+  it "nameがない場合、無効である" do
+    artist_cookie = FactoryBot.build(:my_artist_cookie, name:"")
+    artist_cookie.valid?
+    expect(artist_cookie.errors.of_kind?(:name, :blank)).to be_truthy
+  end
+
+  it "image_urlがない場合、無効である" do
+    artist_cookie = FactoryBot.build(:my_artist_cookie, image_url:"")
+    artist_cookie.valid?
+    expect(artist_cookie.errors.of_kind?(:image_url, :blank)).to be_truthy
   end
 end
