9a4b82a6195339e8fde2558337f8114d6ca96539
akmhmgc <akimuhamaguchi@icloud.com>
2021-08-02 23:06:25 +0900

    アーティストのリストからの削除のテストを追加

--- spec/system/my_list_cookie_spec.rb
+++ spec/system/my_list_cookie_spec.rb
@@ -1,27 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'MyListCookie', type: :system do
-  describe "アーティストの追加" do
-    it "リンクを踏むとアーティストが追加される", js: true do
+  describe "アーティストを検索" do
+    before 'search_for_bz' do
       visit root_path
       fill_in 'search', with: 'Bz'
       find_by_id("search_btn").click
+    end
 
+    it "プラスボタンを押すとアーティストが追加される", js: true do
       find('svg.plus-activated', match: :first).click
       expect(find("#flash")).to have_content 'アーティストが追加されました'
     end
 
-    it "アーティストが5組いると追加されない", js: true do
-      # ５組のアーティストの追加find('svg.svg-inline--fa', match: :first).click
-      visit root_path
-      fill_in 'search', with: 'Bz'
-      find_by_id("search_btn").click
-      5.times do
-        find('svg.plus-activated', match: :first).click
-        expect(page).to have_content 'アーティストが追加されました'
+    context "アーティストが5組追加されている場合" do
+      before "アーティストを5組追加" do
+        5.times do
+          find('svg.plus-activated', match: :first).click
+          expect(page).to have_content 'アーティストが追加されました'
+        end
       end
-      find('svg.plus-activated', match: :first).click
-      expect(find("#flash")).to have_content "追加するためにはリストからアーティストを１組削除してください。"
+      it "さらに追加することが出来ない", js: true do
+        find('svg.plus-activated', match: :first).click
+        expect(find("#flash")).to have_content "追加するためにはリストからアーティストを１組削除してください。"
+      end
+
+      it "マイナスボタンを押すとリストからアーティストが消える", js: true do
+        find('#unlike_heart', match: :first).click
+        expect(find("#flash")).to have_content 'アーティストが削除されました'
+      end
     end
   end
 end
