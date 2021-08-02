ae8d2dbb548f5e4de031730fce137fffcc8d5253
akmhmgc <akimuhamaguchi@icloud.com>
2021-08-02 23:24:19 +0900

    myListモデルのnickname文字数のバリデーションを追加

--- app/models/my_list.rb
+++ app/models/my_list.rb
@@ -3,6 +3,8 @@ class MyList < ApplicationRecord
   generate_public_uid generator: PublicUid::Generators::HexStringSecureRandom.new(20)
   has_many :artists, dependent: :destroy
 
+  validates :nickname,  presence: true, length: { maximum: 10 }
+
   def to_param
     public_uid
   end
