66a2b1c29a91a59ba98f42da91456361adfd7767
akmhmgc <akimuhamaguchi@icloud.com>
2021-09-01 17:54:55 +0900

    不要なルートの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   root to: 'artists#index'
-  resources :myFavArtistLists, controller: :artists, only: [:index, :show]
-  resources :favorite_artists, only: %i[create] do
-    post 'destroy_from_mylist', to: "favorite_artists#destroy_from_mylist", on: :collection
-    post 'render_image', to: "favorite_artists#render_image", on: :collection
-  end
   resources :artists, only: [:index]
 
   # セッションに保存されたアーティストを管理する
   resource :mylist, only: [:create, :destroy] do
-    post 'render', on: :collection
+    post 'render_image', on: :collection
   end
 
   # 出力された画像を表示する
