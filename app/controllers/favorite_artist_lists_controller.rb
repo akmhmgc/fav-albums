44308227ca254a9027a610508249d7b4276ccf83
akmhmgc <akimuhamaguchi@icloud.com>
2021-09-01 18:24:00 +0900

    変数名の修正、コメントの追加

--- app/controllers/favorite_artist_lists_controller.rb
+++ app/controllers/favorite_artist_lists_controller.rb
@@ -1,10 +1,11 @@
 class FavoriteArtistListsController < ApplicationController
   def show
     @my_list = MyList.find_by(public_uid: params[:public_uid])
-    artists_list = @my_list.artists.map(&:name)
-    @description = "#{@my_list.nickname}さんを構成するアーティストは#{artists_list.join('・')}です。"
+    artist_names = @my_list.artists.map(&:name)
+    @description = "#{@my_list.nickname}さんを構成するアーティストは#{artist_names.join('・')}です。"
     return unless flash[:notice]
 
-    @description_twitter = "#{@my_list.nickname}さんを構成するアーティストは%0a#{artists_list.join('%0a')}です。%0a@Buffalo_G_7777 %20%23私を構成する5組のアーティスト%20 #{favorite_artist_list_url(@my_list.to_param)}"
+    # MylistsController#render_imageからredirectした時 = 初めてページが作成された時はtwitterへのシェア用の文章が作成される
+    @description_twitter = "#{@my_list.nickname}さんを構成するアーティストは%0a#{artist_names.join('%0a')}です。%0a@Buffalo_G_7777 %20%23私を構成する5組のアーティスト%20 #{favorite_artist_list_url(@my_list.to_param)}"
   end
 end
