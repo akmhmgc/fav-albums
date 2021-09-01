832de8cbe184d38548f8f797a78886b8d194849f
akmhmgc <akimuhamaguchi@icloud.com>
2021-09-01 16:34:48 +0900

    ルーティングをRESTfulに修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
     post 'destroy_from_mylist', to: "favorite_artists#destroy_from_mylist", on: :collection
     post 'render_image', to: "favorite_artists#render_image", on: :collection
   end
+  resources :artists, only: [:index]
+
+  # セッションに保存されたアーティストを管理する
+  resource :mylist, only: [:create, :destroy] do
+    post 'render', on: :collection
+  end
+
+  # 出力された画像を表示する
+  resources :favorite_artist_lists, only: [:show], param: :public_uid
 end
