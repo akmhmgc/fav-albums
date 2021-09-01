4c356d87fa9e014a4c725f8f7bcb8359b20aeeff
akmhmgc <akimuhamaguchi@icloud.com>
2021-09-01 18:00:29 +0900

    不要なコードを削除

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,7 +2,6 @@ class ArtistsController < ApplicationController
   include SpotifyModule
 
   def index
-    # session[:my_artists_list].clear
     return if params[:search].blank?
 
     searchartists_array = RSpotify::Artist.search(params[:search]).map { |artist| spotify_to_hash(artist) }.compact
